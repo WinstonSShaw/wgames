c2e44773c9cbfb3924a98e4938e4695bbf72cff0
WinstonShaw <winstonsshaw@hotmail.com>
2021-07-04 21:45:44 -0300

    Desafio Estilos y Home

--- src/components/ItemList/ItemListContainer.jsx
+++ src/components/ItemList/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ItemListContainer = (props) => {
-    return <h2 style = {{textAlign: "center"}}>{props.greeting}</h2>;
+    return <h2 className="ItemListContainer">{props.greeting}</h2>;
 };
 
 export default ItemListContainer;
